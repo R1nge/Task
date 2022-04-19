09f5d904ee928c80e80a9426455ae0d2b43d88da
R1nge <r1ngegamedev@gmail.com>
2022-04-19 03:42:53 -0700

    Update sortArray.cpp

--- sortArray.cpp
+++ sortArray.cpp
@@ -73,7 +73,7 @@ void bubbleSort(float* array, int size)
 {
    for(int i = 0; i<size; i++) 
    {
-      int swaps = 0;         //flag to detect any swap is there or not
+      int swaps = 0;      
       for(int j = 0; j<size-i-1; j++) 
       {
          if(array[j] > array[j+1]) 
